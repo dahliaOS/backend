8a687ad23cc22659d96d373ed4c451bc47c6bcd7
LarsB24 <larsb24@outlook.com>
2021-01-21 23:28:30 +0100

    add export

--- lib/dahlia_backend.dart
+++ lib/dahlia_backend.dart
@@ -5,5 +5,6 @@ library dahlia_backend;
 
 export 'src/data_provider.dart';
 export 'src/database_manager.dart';
+export 'src/time_date_manager.dart';
 
 // TODO: Export any libraries intended for clients of this package.
