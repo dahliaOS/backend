91c9cd6ece2ef96a648567fc9f2a139f83318c6c
Lars <larsb24@outlook.com>
2021-02-10 12:35:12 +0100

    Add SystemMouseCursor option

--- lib/src/widgets/box_container.dart
+++ lib/src/widgets/box_container.dart
@@ -1,6 +1,7 @@
 import 'package:dahlia_backend/src/data/data_management.dart';
 import 'package:dahlia_backend/src/widgets/blur/blur.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 
 class BoxContainer extends StatelessWidget {
@@ -12,6 +13,7 @@ class BoxContainer extends StatelessWidget {
   final BorderRadius? customBorderRadius;
   final bool useSystemOpacity;
   final double? customBlur;
+  final SystemMouseCursor cursor;
 
   BoxContainer({
     this.child,
@@ -24,25 +26,30 @@ class BoxContainer extends StatelessWidget {
     this.customBorderRadius,
     this.customBlur,
     this.useSystemOpacity = false,
+    this.cursor = SystemMouseCursors.basic,
     Key? key,
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     final _data = context.read<PreferenceProvider>();
-    return Container(
-      width: width,
-      height: height,
-      margin: margin,
-      decoration: BoxDecoration(borderRadius: customBorderRadius),
-      child: Blur(
-        useBlur: useBlur ?? true,
-        customBlur: customBlur,
-        customBorderRadius: customBorderRadius,
-        child: Container(
-          child: child,
-          padding: padding,
-          color:
-              useSystemOpacity ? color?.withOpacity(_data.themeOpacity) : color,
+    return MouseRegion(
+      cursor: cursor,
+      child: Container(
+        width: width,
+        height: height,
+        margin: margin,
+        decoration: BoxDecoration(borderRadius: customBorderRadius),
+        child: Blur(
+          useBlur: useBlur ?? true,
+          customBlur: customBlur,
+          customBorderRadius: customBorderRadius,
+          child: Container(
+            child: child,
+            padding: padding,
+            color: useSystemOpacity
+                ? color?.withOpacity(_data.themeOpacity)
+                : color,
+          ),
         ),
       ),
     );
